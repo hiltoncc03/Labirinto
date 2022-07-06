3a7525512142ac4acf8ac875bb035932e92415bb
Hilton <hiltoncarrara003@gmail.com>
2022-07-05 21:29:04 -0300

    ETAPA 2

--- labirinto.cpp
+++ labirinto.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
  
 int main() {
@@ -41,17 +41,37 @@ int main() {
         }
         caractere = fgetc(file);
     }
-    
+    //SEGUNDA ETAPA
+    queue<pair<int,int>> coord;
+    coord.push({1,1});
+    i=2;
+    L[1][1]=1;
+    while(!coord.empty()){//Loop que funciona enquanto a fila não estiver vazia; A fila armazena os valores das coordenadas, 1 por vez
+        if(L[coord.front().first-1][coord.front().second]==0){//Analizando se a coordenada acima da atual é = 0
+            coord.push({coord.front().first-1, coord.front().second});
+            L[coord.front().first-1][coord.front().second] = L[coord.front().first][coord.front().second]+1;
+        }
+        if(L[coord.front().first+1][coord.front().second]==0){//Analizando se a coordenada abaixo da atual é = 0
+            coord.push({coord.front().first+1, coord.front().second});
+            L[coord.front().first+1][coord.front().second] = L[coord.front().first][coord.front().second]+1;
+        }
+        if(L[coord.front().first][coord.front().second-1]==0){//Analizando se a coordenada à esquerda da atual é = 0
+            coord.push({coord.front().first, coord.front().second-1});
+            L[coord.front().first][coord.front().second-1] = L[coord.front().first][coord.front().second]+1;
+        }
+        if(L[coord.front().first][coord.front().second+1]==0){//Analizando se a coordenada à direita da atual é = 0
+            coord.push({coord.front().first, coord.front().second+1});
+            L[coord.front().first][coord.front().second+1] = L[coord.front().first][coord.front().second]+1;
+        }
+        coord.pop();
+        i++;
+    }
+
     //Imprime o labirinto
     cout << endl << endl << endl;
     for(j=0;j<n;j++){
         for(i=0;i<n;i++){
-            if(L[i][j]== -1){
-                cout << L[i][j] << ' ';
-            }
-            else{
-               cout << ' ' << L[i][j] << ' '; 
-            }
+            printf("%3d",L[i][j] );
         }
         cout << endl;
     }
